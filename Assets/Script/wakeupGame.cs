69b8ab6078ae9fb62cc04658da96c7696b9d9f53
Weihao Yan (RIT Student) <wy2751@ad.rit.edu>
2019-01-26 12:10:20 -0500

    wakeGame

--- Assets/Script/wakeupGame.cs
+++ Assets/Script/wakeupGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
 
 public class wakeupGame : MonoBehaviour {
 
@@ -13,12 +14,19 @@ public class wakeupGame : MonoBehaviour {
     public Text text1;
     public float fireIndex = 4.0f;
     private bool forWakeUp = true;
+
+    private ColorCorrectionCurves _colorCor;
+
     // Use this for initialization
     void Start () {
         rangePosition = new Vector3(Random.Range(transform.position.x, transform.position.x + 12), wakeupBar.transform.position.y,wakeupBar.transform.position.z);
         worldRangePosition = Camera.main.ViewportToWorldPoint(rangePosition);
         wakeUpArea = GameObject.Instantiate(wakeupRange, rangePosition, wakeupRange.transform.rotation);
         wakeUpArea.SetActive(true);
+        if(_colorCor == null){
+            _colorCor = FindObjectOfType<ColorCorrectionCurves>();
+        }
+        
 	}
 	
 	// Update is called once per frame
@@ -29,10 +37,15 @@ public class wakeupGame : MonoBehaviour {
 
     void wakeUp(){
         if (Input.GetMouseButtonDown(0)) {
-            if(transform.position.x <= (rangePosition.x+0.5) && (transform.position.x >= rangePosition.x-0.5)){
+            if (transform.position.x <= (rangePosition.x + 0.5) && (transform.position.x >= rangePosition.x - 0.5))
+            {
                 text1.text = "Wake Up Successfully!!!";
             }
-            else text1.text = "GG!!!!";
+            else
+            {
+                text1.text = "GG!!!!";
+                sleepAgain();
+            }
 
             forWakeUp = false;
 
@@ -50,5 +63,9 @@ public class wakeupGame : MonoBehaviour {
         gameObject.SetActive(false);
         Destroy(wakeUpArea);
         text1.text = "";
+    }
+
+    void sleepAgain(){
+        _colorCor.saturation = 0.0f;
     }
 }
