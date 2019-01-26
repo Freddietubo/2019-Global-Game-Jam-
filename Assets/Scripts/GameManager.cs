0273bf0aaab0530d0fe5731697f35d5c64e0d75c
Ollyting Xin (RIT Student) <ox7313@ad.rit.edu>
2019-01-25 23:33:29 -0500

    basic GM

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { playing, won, lost, pause };
+
+public class GameManager : MonoBehaviour {
+
+    private GameObject GM;
+
+    private GameState gameState;
+
+    private GameObject mainCanvas;
+
+    private GameObject menuCanvas;
+
+    private GameObject mainCamera;
+
+    public GameObject playerPrefab;
+
+    public float insPosX;
+
+    public float insPosY;
+
+    void Awake()
+    {
+        GM = GameObject.FindGameObjectWithTag("Game Manager");
+
+        mainCanvas = GameObject.FindGameObjectWithTag("Main Canvas");
+        menuCanvas = GameObject.FindGameObjectWithTag("Menu Canvas");
+
+        mainCamera = GameObject.FindGameObjectWithTag("Main Camera");
+
+    }
+
+    void Start()
+    {
+        SetGameState(GameState.playing);
+
+        GameObject playerInstance = Instantiate(playerPrefab, new Vector3(insPosX, insPosY, 0), Quaternion.identity) as GameObject;
+
+        mainCanvas.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetGameState(GameState state)
+    {
+        this.gameState = state;
+        OnStateChange();
+    }
+
+    private void OnStateChange()
+    {
+        this.mainCanvas.SetActive(false);
+        this.menuCanvas.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
