0ee996be94f6eaa5307aea6482bc4d7d6f0d9057
Yun Jiang (RIT Student) <yj9812@ad.rit.edu>
2019-01-26 23:16:48 -0500

    reviseGame

---
+++ Assets/Script/ClickingJudgement.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickingJudgement : MonoBehaviour {
+    public Button[] key;
+    public GameObject winText;
+    public GameObject  failText;
+    public TimeCountdownFunction timeCountdown;
+    //keyI = 0, keyG = 1, keyM = 2
+    int count = 0;
+    private int[] countIndex1 = new int[3];
+    private int[] rightCountIndex1 = { 0, 1, 2 };
+    private int keyLength1;
+    private bool isEnd = false;
+
+
+	// Use this for initialization
+	void Start () {
+        keyLength1 = rightCountIndex1.Length;
+        key[0].transform.position = new Vector3(Random.Range(90.0f, 815.0f), Random.Range(92.0f, 419.0f), 0.0f);
+        key[1].transform.position = new Vector3(Random.Range(90.0f, 815.0f), Random.Range(92.0f, 419.0f), 0.0f);
+        key[2].transform.position = new Vector3(Random.Range(90.0f, 815.0f), Random.Range(92.0f, 419.0f), 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        isEnd = timeCountdown.countdownValue();
+
+        if ((count == keyLength1)&&(isEnd == false))
+        {
+
+            if (compareArr(countIndex1, rightCountIndex1))
+            {
+                //Debug.Log("Right");
+                winText.SetActive(true);
+                count = 0;
+
+            }
+            else
+            {
+                failText.SetActive(true);
+                count = 0;
+            }
+        }
+         if ((count == keyLength1) && (isEnd == true)) {
+            failText.SetActive(true);
+            count = 0;
+
+        }
+		
+            
+            
+        
+	}
+
+    public void PressKeyI()
+    {
+        if(count != keyLength1)
+        {
+          
+            countIndex1[count] = 0;
+            count++;
+        }
+        Debug.Log(count);
+        Debug.Log(countIndex1[count - 1]);
+
+    }
+
+    public void PressKeyG()
+    {
+        if (count != keyLength1 )
+        {
+            countIndex1[count] = 1;
+            count++;
+        }
+        Debug.Log(count);
+        Debug.Log(countIndex1[count - 1]);
+    }
+
+    public void PressKeyM()
+    {
+        if(count != keyLength1)
+        {
+            countIndex1[count] = 2 ;
+            count++;
+        }
+        Debug.Log(count);
+
+        Debug.Log(countIndex1[count -1]);
+        
+    }
+    public static bool compareArr(int[] arr1, int[] arr2)
+    {
+
+        bool[] flag = new bool[arr1.Length];//初始化一个bool数组，初始值全为false；
+
+        for (int i = 0; i < arr1.Length; i++)
+        {    
+            if (arr1[i] == arr2[i]) flag[i] = true;//遇到有相同的值，对应的bool数组的值设为true；
+         }
+
+        foreach (var item in flag)
+        {
+
+            if (item == false) return false; //遍历bool数组，还有false，就说明有不同的值，结果返回false。
+
+        }
+
+        return true;
+
+    }
+
+
+
+}
---
+++ Assets/Script/TimeCountdownFunction.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountdownFunction : MonoBehaviour {
+
+    public int testTotalSeconds = 10;
+    private int leaveSeconds;
+    private bool onCountDown = false;
+    private string countDownTitle = "Start";
+
+    private bool isEnd;
+    void Awake()
+    {
+        leaveSeconds = testTotalSeconds;
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(50, 50, 50, 50), leaveSeconds.ToString());// 倒计时秒数 //
+
+
+        //if (GUI.Button(new Rect(150, 50, 80, 30), countDownTitle))
+        //{
+        //    if (countDownTitle == "Start")
+        //    {
+        //        countDownTitle = "Pause";
+        //        onCountDown = true;
+        //        StartCoroutine(DoCountDown());
+        //    }
+        //    else
+        //    {
+        //        countDownTitle = "Start";
+        //        onCountDown = false;
+        //        StopAllCoroutines();
+        //    }
+        //}
+    }
+    // Use this for initialization
+    void Start()
+    {
+        onCountDown = true;
+        StartCoroutine(DoCountDown());
+        isEnd = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator DoCountDown()
+    {
+        while (leaveSeconds > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            leaveSeconds--;
+        }
+
+        if(leaveSeconds == 0)
+        {
+            isEnd = true;
+            
+            
+        }
+
+    }
+
+    public bool countdownValue()
+    {
+        return isEnd;
+    }
+}
